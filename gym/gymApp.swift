d34f7b5d8aade14c0546742fc5ffbd4df13e54eb
Jake Watembach <jakewatembach@gmail.com>
2023-03-12 22:37:13 -0600

    Initial Commit

---
+++ gym/gymApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymApp.swift
+//  gym
+//
+//  Created by Jake Watembach on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct gymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
